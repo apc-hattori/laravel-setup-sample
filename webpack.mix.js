e33c4f93de769594dfb519c8740c73c9fd353b77
Kazuya HATTORI <k_hattori@ap-com.co.jp>
2017-05-16 13:51:14 +0900

    開発者モードでデバッグできるようにSource Mapを生成できる設定追加

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,8 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .sourceMaps();
 if (mix.config.inProduction) {
     mix.version();
 }
